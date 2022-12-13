e83c59b1068b3fc375fdc69a150c7a250b3d90d8
pekos25 <pekos25@gmail.com>
2022-12-13 10:27:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import { Container, Row, Col } from "react-bootstrap";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
